cfa08d060801b1ed4835f6c94dd02a7b83470942
Wasim Algamal <walgamal123@gmail.com>
2023-07-04 20:38:03 -0400

    Init Commit - Created App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import HomeHero from './Components/HomeHero'
+
+function App() {
+  return (
+    <>
+      <HomeHero />
+      {/* <HomeHeader />
+      <Body />
+      <Footer /> */}
+    </>
+  )
+}
+
+export default App
