a52ea03874b5d830d9e1bab57fabf43d49dacbf6
Wasim Algamal <walgamal123@gmail.com>
2023-07-12 20:10:00 -0400

    Started Home Page - Created Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import HomeHero from './Components/HomeHero'
+import HomeHeader from './Components/HomeHeader'
+import HomeFooter from './Components/HomeFooter'
 
 function App() {
   return (
     <>
+      <HomeHeader />
       <HomeHero />
-      {/* <HomeHeader />
-      <Body />
-      <Footer /> */}
+      <HomeFooter />
     </>
   )
 }
