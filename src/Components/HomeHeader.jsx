9d5330060ea45bec2103a1ff6bd9061da8839927
Wasim Algamal <walgamal123@gmail.com>
2023-07-14 23:02:17 -0400

    Added alt text for header images

--- src/Components/HomeHeader.jsx
+++ src/Components/HomeHeader.jsx
@@ -5,12 +5,12 @@ function Header() {
   return (
     <div className='header-container'>
         <div className='site-logo-container'>
-          <img className='site-logo' src='/assets/site-logo.svg'/>
+          <img className='site-logo' src='/assets/site-logo.svg' alt='site-logo' />
           <h1 className='site-title'>WHIZDUMB</h1>
         </div>
         <div className='menu-container'>
           <h1 className='menu-title'>MENU</h1>
-          <img className='hamburger-menu-icon' src='/assets/hamburger-menu-icon.svg'/>
+          <img className='hamburger-menu-icon' src='/assets/hamburger-menu-icon.svg' alt='hamburger-menu-icon' />
         </div>
     </div>
   )
