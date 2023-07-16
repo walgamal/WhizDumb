300ae90fcb7498422d624319a2962ecdeda419e5
Wasim Algamal <walgamal123@gmail.com>
2023-07-16 16:13:57 -0400

    Created Instructions Page with route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './Pages/Home.jsx'
 import About from './Pages/About.jsx'
 import Contact from './Pages/Contact.jsx'
+import Instructions from "./Pages/Instructions.jsx";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/instructions" element={<Instructions />} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/Instructions.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Instructions() {
+  return (
+    <div>Instructions</div>
+  )
+}
+
+export default Instructions
