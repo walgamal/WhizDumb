7734a35405bdaf16d7a7f7415c7130fb078bdbdb
Wasim Algamal <walgamal123@gmail.com>
2023-07-21 17:10:26 -0400

    Created misc card component, Added to about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MiscHeader from '../Components/MiscComponents/MiscHeader'
 import MiscFooter from '../Components/MiscComponents/MiscFooter'
+import MiscCard from '../Components/MiscComponents/MiscCard'
 
 function About() {
   return (
     <>
       <MiscHeader />
+      <MiscCard />
       <MiscFooter />
     </>
   )
