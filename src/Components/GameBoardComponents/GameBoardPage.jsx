db3b08216ed2384ae560d3fdf0143a11cceab5f4
Wasim Algamal <walgamal123@gmail.com>
2024-04-23 22:34:15 -0400

    Added Winning Logic

--- src/Components/GameBoardComponents/GameBoardPage.jsx
+++ src/Components/GameBoardComponents/GameBoardPage.jsx
@@ -4,19 +4,53 @@ import MiscFooter from '../MiscComponents/MiscFooter'
 import GameBoard from '../GameBoardComponents/GameBoard'
 import ScoreCards from './ScoreCards'
 import QuestionModal from './QuestionModal'
+import WinnerModal from './WinnerModal'
 import AppContext from '../../AppContext'
 
 function GameBoardPage() {
-  const { players } = useContext(AppContext);
-  const { currentPlayer } = useContext(AppContext);
-  const { scores } = useContext(AppContext);
-  const { categories } = useContext(AppContext);
+  const { players, currentPlayer, scores, categories, questionsAnswered } = useContext(AppContext);
 
   const [openModal, setOpenModal] = useState(false);
   const [counter, setCounter] = useState(-1);
 
+  const [winner, setWinner] = useState(null);
+  const [winnerScore, setWinnerScore] = useState(null);
+
   const setCounterToNum = (num) => {
     setCounter(num);
+  }
+
+  const allQuestionsAnswered = () => {
+    for (let row of questionsAnswered) {
+      for (let cell of row) {
+        if (cell === 0) {
+          return false;
+        }
+      }
+    }
+    
+    return true;
+  };
+
+  const calculateWinner = () => {
+    let maxScore = -1;
+    let playerWithMostPoints = null;
+
+    scores.forEach((score, index) => {
+      if (score > maxScore) {
+        maxScore = score;
+        playerWithMostPoints = players[index];
+        setWinner(playerWithMostPoints);
+      }
+    });
+    
+    setWinnerScore(maxScore);
+  };
+
+  const handleClearedBoard = () => {
+    if (allQuestionsAnswered()) {
+      calculateWinner();
+    } 
   }
 
   const handleQuestionIconClick = () => {
@@ -25,22 +59,33 @@ function GameBoardPage() {
     setCounter(10);
   };
 
-  const handleClosingModal = () => {
+  const handleClosingQuestionModal = () => {
     console.log("...Modal closed"); 
     setOpenModal(false)
+    handleClearedBoard();
   }
 
   return (
     <>
         <QuestionModal 
           open={openModal} 
-          onClose={handleClosingModal} 
+          onClose={handleClosingQuestionModal} 
           counter={counter} 
           setCounter={setCounterToNum}
         />
         
         <MiscHeader />
-        <GameBoard handleClick={handleQuestionIconClick} categories={categories} />
+
+        {(!winner) && <GameBoard handleClick={handleQuestionIconClick} categories={categories} />}
+        
+        {
+          (winner) && 
+          <WinnerModal 
+            winner={winner} 
+            points={winnerScore}
+          />
+        }
+
         <ScoreCards players={players} currentPlayer={currentPlayer} scores={scores}/>
         <MiscFooter />
     </>
---
+++ src/Components/GameBoardComponents/WinnerModal.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './../../ComponentsCSS/GameBoardPageCSS/QuestionModal.css';
+
+function WinnerModal(props) {
+  return (
+    <>
+      <div className='modal-overlay'>
+        <div className='modal-container'>
+          <h1 className='modal-title'>You're The Whiz!</h1>
+          <p className='modal-question'>Congratulations, {props.winner}! You won and scored {props.points} points.</p>
+          <br />
+          <p className='modal-question'>You have earned the right to call everyone else Dumb!</p>
+          <br />
+
+          <div className='modal-button-container'>
+              <div className='modal-top-buttons-container'>
+                <div>
+                    <button className='modal-option'>Play Again</button>
+                </div>
+                <div>
+                    <button className='modal-option'>Reset Game</button>
+                  </div>
+              </div>
+            </div>
+          
+          {/* TODO: Add 'Play Again' and 'Reset' buttons */}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default WinnerModal;
